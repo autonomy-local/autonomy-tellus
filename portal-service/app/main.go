96e65fd4a8c1970a209fe4273887f3ceb9ea8b4d
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-05-14 22:36:14 +0900

    feat: add hello world api

---
+++ portal-service/app/main.go
@@ -0,0 +1,15 @@
+// add portal app backend api
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
